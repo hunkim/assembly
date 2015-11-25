80d469fad5b56549264d9e1f4f395a62837c3221
Sung Kim <hunkim@gmail.com>
2015-11-25 11:14:54 +0800

    Added

--- getBill.php
+++ getBill.php
@@ -2,49 +2,51 @@
 
 include 'Actor.php';
 
-// http://likms.assembly.go.kr/bill/jsp/CoactorListPopup.jsp?bill_id=PRC_A1J5J1E1N1K0Q1O4A4V8L1H4C2Q4C9
-$url = "http://likms.assembly.go.kr/bill/jsp/SummaryPopup.jsp?bill_id=";
-$build = "PRC_A1J5J1E1N1K0Q1O4A4V8L1H4C2Q4C9";
+getBillId("PRC_A1J5J1E1N1K0Q1O4A4V8L1H4C2Q4C9");
+
+function getBill($billId) {
+  // http://likms.assembly.go.kr/bill/jsp/CoactorListPopup.jsp?bill_id=PRC_A1J5J1E1N1K0Q1O4A4V8L1H4C2Q4C9
+  $url = "http://likms.assembly.go.kr/bill/jsp/SummaryPopup.jsp?bill_id=";
+  //.DS_Store$build = "PRC_A1J5J1E1N1K0Q1O4A4V8L1H4C2Q4C9";
 
 
-if (false) {
-$ch = curl_init();
-curl_setopt( $ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows; U; Windows NT 5.1; rv:1.7.3) Gecko/20041001 Firefox/0.10.1" );
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_HEADER, false);
-curl_setopt($ch, CURLOPT_URL, ($url . $build));
+  if (false) {
+    $ch = curl_init();
+    curl_setopt( $ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows; U; Windows NT 5.1; rv:1.7.3) Gecko/20041001 Firefox/0.10.1" );
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HEADER, false);
+    curl_setopt($ch, CURLOPT_URL, ($url . $build));
 
-$content = curl_exec($ch);
-curl_close($ch);
-echo $url . $build;
-echo iconv('EUC-KR', 'UTF-8', $content);
-exit(0);
-} else {
-$content = file_get_contents ("bill.html");
-}
-
-$txt = strip_tags($content);
-$tokens = preg_split('/\n+/', $txt);
-
-$idx = 0;
-$title = "";
-$summary = "";
-foreach ($tokens as $line) {
-  $line = trim($line); // let's trim
-  
-  if ($line=="" || strpos($line, "제안이유")!==false || strpos($line, "http://")!==false) {
-    continue;
-  }
-
-  // assume the first text is the title
-  if ($idx++==0) {
-    $title = $line;
+    $content = curl_exec($ch);
+    curl_close($ch);
+    echo $url . $build;
+    echo iconv('EUC-KR', 'UTF-8', $content);
   } else {
-    $summary .= "$line\n";
+    $content = file_get_contents ("bill.html");
   }
+
+  $txt = strip_tags($content);
+  $tokens = preg_split('/\n+/', $txt);
+
+  $idx = 0;
+  $title = "";
+  $summary = "";
+  foreach ($tokens as $line) {
+    $line = trim($line); // let's trim
+
+    if ($line=="" || strpos($line, "제안이유")!==false || strpos($line, "http://")!==false) {
+      continue;
+    }
+
+    // assume the first text is the title
+    if ($idx++==0) {
+      $title = $line;
+    } else {
+      $summary .= "$line\n";
+    }
+  }
+
+  $bill = new Bill($title, $summary);
+  return $bill;
 }
-
-echo("Title: $title\n");
-echo("Summary: $summary");
-
 ?>
--- getCoActors.php
+++ getCoActors.php
@@ -2,47 +2,54 @@
 
 include 'Actor.php';
 
-// http://likms.assembly.go.kr/bill/jsp/CoactorListPopup.jsp?bill_id=PRC_A1J5J1E1N1K0Q1O4A4V8L1H4C2Q4C9
-$url = "http://likms.assembly.go.kr/bill/jsp/CoactorListPopup.jsp?bill_id=";
-$build = "PRC_A1J5J1E1N1K0Q1O4A4V8L1H4C2Q4C9";
-
-
-if (false) {
-$ch = curl_init();
-curl_setopt( $ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows; U; Windows NT 5.1; rv:1.7.3) Gecko/20041001 Firefox/0.10.1" );
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_HEADER, false);
-curl_setopt($ch, CURLOPT_URL, ($url . $build));
-
-$content = curl_exec($ch);
-curl_close($ch);
-echo $url . $build;
-echo iconv('EUC-KR', 'UTF-8', $content);
-} else {
-$content = file_get_contents ("co.html");
+// for testing
+if ($argv[0]=='getCoActors.php') {
+  getActors("PRC_A1J5J1E1N1K0Q1O4A4V8L1H4C2Q4C9");
 }
 
-$txt = strip_tags($content);
-$tokens = preg_split('/\s+/', $txt);
+function getActors($billid) {
+  // http://likms.assembly.go.kr/bill/jsp/CoactorListPopup.jsp?bill_id=PRC_A1J5J1E1N1K0Q1O4A4V8L1H4C2Q4C9
+  $url = "http://likms.assembly.go.kr/bill/jsp/CoactorListPopup.jsp?bill_id=";
+  $build = "PRC_A1J5J1E1N1K0Q1O4A4V8L1H4C2Q4C9";
 
-$propsed = false;
-foreach ($tokens as $value) {
-  echo("[$value]\n");
-  if ($value=="발의의원") {
-    echo("Here!");
-    $propsed = true;
-    continue;
+
+  if (false) {
+    $ch = curl_init();
+    curl_setopt( $ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows; U; Windows NT 5.1; rv:1.7.3) Gecko/20041001 Firefox/0.10.1" );
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HEADER, false);
+    curl_setopt($ch, CURLOPT_URL, ($url . $build));
+
+    $content = curl_exec($ch);
+    curl_close($ch);
+    echo $url . $build;
+    echo iconv('EUC-KR', 'UTF-8', $content);
+  } else {
+    $content = file_get_contents ("co.html");
   }
 
-  if ($propsed && strpos($value, ')')) {
-    $namearr = parse_names($value);
-  }
-}
+  $txt = strip_tags($content);
+  $tokens = preg_split('/\s+/', $txt);
 
-foreach ($namearr as $value) {
-  if ($value!='') {
-    $a = new Actor($value);
-    echo ($a->toString() . "\n");
+  $propsed = false;
+  foreach ($tokens as $value) {
+    echo("[$value]\n");
+    if ($value=="발의의원") {
+      echo("Here!");
+      $propsed = true;
+      continue;
+    }
+
+    if ($propsed && strpos($value, ')')) {
+      $namearr = parse_names($value);
+    }
+  }
+
+  foreach ($namearr as $value) {
+    if ($value!='') {
+      $a = new Actor($value);
+      echo ($a->toString() . "\n");
+    }
   }
 }
 
