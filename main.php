d443592e5e30e02a6770858d09f4abbb91e0e48b
Sung Kim <hunkim@gmail.com>
2015-11-25 11:44:03 +0800

    Added

---
+++ main.php
@@ -0,0 +1,26 @@
+<?php
+
+// Need to get it from somewhere
+$billId = "PRC_A1J5J1E1N1K0Q1O4A4V8L1H4C2Q4C9"
+
+$bill = getBill($billId);
+
+// open DB
+$db = new mysqli("p:localhost", "trend", "only!trend!", "assembly");
+// Check connection
+if ($db->connect_error) {
+    die("Connection failed: " . $db->connect_error);
+}
+
+// insert bill to DB
+$bill->insert($db);
+
+$actors = getActors($billId);
+
+foreach ($actors as $a) {
+  $a->insert($db);
+  $a->insertWithBill($db, $bill->id);
+}
+
+$db->close();
+?>
