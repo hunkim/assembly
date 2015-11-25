bcd9bc52ee1d54aa806496cbfb5b1fc5d11e2a70
Sung Kim <hunkim@gmail.com>
2015-11-25 15:44:36 +0800

    Added

--- Bill.php
+++ Bill.php
@@ -3,7 +3,7 @@
 
 if ($argv[0]=='Bill.php') {
   $b = new getBill("","");
-  $b->id = "PRC_A1J5J1E1N1K0Q1O4A4V8L1H4C2Q4C9");
+  $b->id = "PRC_A1J5J1E1N1K0Q1O4A4V8L1H4C2Q4C9";
 
   / open DB
   $db = new mysqli("p:localhost", "trend", "", "assembly");
