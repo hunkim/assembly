68fcb314b43ba1a79d5ac4e05b36a573685c631c
Sung Kim <hunkim@gmail.com>
2015-11-25 12:36:42 +0800

    Added

--- getCoActor.php
+++ getCoActor.php
@@ -16,7 +16,7 @@ function getActors($billid) {
   $build = "PRC_A1J5J1E1N1K0Q1O4A4V8L1H4C2Q4C9";
 
 
-  if (false) {
+  if (FALSE) {
     $ch = curl_init();
     curl_setopt( $ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows; U; Windows NT 5.1; rv:1.7.3) Gecko/20041001 Firefox/0.10.1" );
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -34,12 +34,18 @@ function getActors($billid) {
   $txt = strip_tags($content);
   $tokens = preg_split('/\s+/', $txt);
 
-  $propsed = false;
+  $propsed = FALSE;
   foreach ($tokens as $value) {
     echo("[$value]\n");
     if ($value=="발의의원") {
       echo("Here!");
-      $propsed = true;
+      $propsed = TRUE;
+      continue;
+    }
+
+    if ($value=="찬성의원") {
+      echo("Agrred!");
+      $propsed = FALSE;
       continue;
     }
 
@@ -51,7 +57,7 @@ function getActors($billid) {
   $actorArr = [];
   foreach ($namearr as $value) {
     if ($value!='') {
-      $a = new Actor($value);
+      $a = new Actor($proposed, $value);
       $actorArr[] = $a;
     }
   }
