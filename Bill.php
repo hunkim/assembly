079e9122e7b802b52c23bc0b560d9dc945862138
Sung Kim <hunkim@gmail.com>
2015-11-25 15:59:43 +0800

    Added

--- Bill.php
+++ Bill.php
@@ -43,7 +43,7 @@ class Bill {
 
 
   function exist($db) {
-    $sql = ""SELECT id FROM HTML WHERE id='". $db->real_escape_string($this->id) . "'";
+    $sql = "SELECT id FROM HTML WHERE id='". $db->real_escape_string($this->id) . "'";
     $result = $db->query($sql);
     echo ($sql);
     return ($result!==false && $result->num_rows > 0);
