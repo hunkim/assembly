e2a2f5c3d0444f58c3ccd3c233f6b0ff882a972a
Sung Kim <hunkim@gmail.com>
2015-11-25 08:17:08 +0800

    Added php

---
+++ CoactorList.php
@@ -0,0 +1,10 @@
+<?php
+
+// http://likms.assembly.go.kr/bill/jsp/CoactorListPopup.jsp?bill_id=PRC_A1J5J1E1N1K0Q1O4A4V8L1H4C2Q4C9
+$url = "http://likms.assembly.go.kr/bill/jsp/CoactorListPopup.jsp?bill_id=";
+$bilid = "PRC_A1J5J1E1N1K0Q1O4A4V8L1H4C2Q4C9";
+
+
+$content = http_get($url, $bilid);
+echo $content;
+?>
