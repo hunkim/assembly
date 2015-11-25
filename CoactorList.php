b3f4097292ae79f251c33f815fa9e1849b04081b
Sung Kim <hunkim@gmail.com>
2015-11-25 08:44:38 +0800

    Added

--- CoactorList.php
+++ CoactorList.php
@@ -2,13 +2,18 @@
 
 // http://likms.assembly.go.kr/bill/jsp/CoactorListPopup.jsp?bill_id=PRC_A1J5J1E1N1K0Q1O4A4V8L1H4C2Q4C9
 $url = "http://likms.assembly.go.kr/bill/jsp/CoactorListPopup.jsp?bill_id=";
-$bilid = "PRC_A1J5J1E1N1K0Q1O4A4V8L1H4C2Q4C9";
+$build = "PRC_A1J5J1E1N1K0Q1O4A4V8L1H4C2Q4C9";
 
 
 $ch = curl_init();
 
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HEADER, false);
 curl_setopt($ch, CURLOPT_URL, ($url . $build));
 
 $content = curl_exec($ch);
+curl_close($ch);
+
 echo $content;
+
 ?>
