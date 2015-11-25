9f3b40baebadf946224ee244768502e24e4a41e7
Sung Kim <hunkim@gmail.com>
2015-11-25 10:26:02 +0800

    Added

--- Actor.php
+++ Actor.php
@@ -1,14 +1,22 @@
 <?php
 
 class Actor {
-  var $edible;
-  var $color;
+  var $name;
+  var $cname;
+  var $party;
 
   // 안규백(새정치민주연합/安圭伯)
   function Actor($str) {
     echo ("Parsing $str\n");
-    $arr = preg_split('/\/)/', $str);
-    print_r($arr);
+    $arr = explode('(', $str);
+    assert(count($arr)!=2);
+
+    $this->$name = $arr[0];
+    $arr2 = explode('/', $arr[1]);
+    assert(count($arr2)!=2);
+
+    $this->party = $arr2[0];
+    $this->cname = $arr2[1];
   }
 }
 
