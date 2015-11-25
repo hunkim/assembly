09721bc62573081488dc13252d2d23e1c03621a4
Sung Kim <hunkim@gmail.com>
2015-11-25 10:17:06 +0800

    Added

---
+++ Actor.php
@@ -0,0 +1,14 @@
+<?php
+
+class Actor {
+  var $edible;
+  var $color;
+
+  // 안규백(새정치민주연합/安圭伯)
+  function Vegetable($str) {
+    $arr = preg_split('/\/)/', $str);
+    print_r($arr);
+  }
+}
+
+?>
