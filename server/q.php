031a730f3ccc301a4855b2bb7a5a4bb087adeaec
Sung Kim <hunkim@gmail.com>
2015-11-26 11:15:46 +0800

    Added

--- server/q.php
+++ server/q.php
@@ -821,7 +821,7 @@ function processQuery($apptype, $sql) {
 
             // Check it's ready to be added
             if ($idx%10==0) {
-              $rows[]=['children'=>$child];
+              $rows[]=['name'=>$idx, 'children'=>$child];
               $child = [];
             }
           }
@@ -844,7 +844,7 @@ function processQuery($apptype, $sql) {
 
     // Add last one
     $child[] = $data;
-    $rows[]=['children'=>$child];
+    $rows[]=['name'=>$idx,'children'=>$child];
 
   } else {
     while($row = $result->fetch_array(MYSQLI_ASSOC)) {
