4080c6ae6e280268fd3d5c3511b3d0cdffc9ca0a
Sung Kim <hunkim@gmail.com>
2015-11-27 08:26:43 +0800

    Added

--- server/q.php
+++ server/q.php
@@ -55,7 +55,7 @@ switch($apptype) {
 
   // This is for the autocomplete search
   case 'actor':
-    $sql = "select CONCAT_WS('(', name, CONCAT(CONCAT_WS('/', cname, party)),')') as info, id from Actor order by name";
+    $sql = "select CONCAT_WS('(', name,   CONCAT(CONCAT_WS('/', cname, party),')')              ) as info, id from Actor order by name";
     break;
 
   default:
