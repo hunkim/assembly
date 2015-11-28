269ad1cca8ba105a4d200ddaa2866e21649a88d3
Sung Kim <hunkim@gmail.com>
2015-11-28 18:18:04 +0800

    up

--- server/q.php
+++ server/q.php
@@ -172,10 +172,29 @@ function processQuery($apptype, $sql) {
   print (json_encode($rows));
 
   $error = json_last_error();
-  if ($debug) {
-    print ($error);
-  }
-
+  switch (json_last_error()) {
+          case JSON_ERROR_NONE:
+              echo ' - No errors';
+          break;
+          case JSON_ERROR_DEPTH:
+              echo ' - Maximum stack depth exceeded';
+          break;
+          case JSON_ERROR_STATE_MISMATCH:
+              echo ' - Underflow or the modes mismatch';
+          break;
+          case JSON_ERROR_CTRL_CHAR:
+              echo ' - Unexpected control character found';
+          break;
+          case JSON_ERROR_SYNTAX:
+              echo ' - Syntax error, malformed JSON';
+          break;
+          case JSON_ERROR_UTF8:
+              echo ' - Malformed UTF-8 characters, possibly incorrectly encoded';
+          break;
+          default:
+              echo ' - Unknown error';
+          break;
+      }
 	$conn->close();
 }
 ?>
