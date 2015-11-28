acd4873abf16a295017bd1ef79a399c1a59b04d8
Sung Kim <hunkim@gmail.com>
2015-11-28 17:57:11 +0800

    Refacoting using JSon

--- server/q.php
+++ server/q.php
@@ -157,7 +157,9 @@ function processQuery($apptype, $sql) {
     // Should start with childeren
     $rows = ['name'=>'all', 'children'=>$rows];
   } else {
+
     while($row = $result->fetch_array(MYSQLI_ASSOC)) {
+      echo ("how?");
       $rows[] = $row;
 
     }
