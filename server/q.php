021bb7067ba6c17d14bbc4c563aa34a0377f7448
Sung Kim <hunkim@gmail.com>
2015-11-28 19:35:15 +0800

    up

--- server/q.php
+++ server/q.php
@@ -46,8 +46,8 @@ switch($apptype) {
   case 'coact':
    $sql = "select name, cname, party, actorid as id, count(actorid) c from CoActor c ".
           " inner join Actor a on a.id=actorid inner join  (select distinct(billid) from CoActor ".
-          " where actorid = ? $optQuery) x on x.billid=c.billid group by actorid order by c desc limit 11;";
-    break;
+          " where actorid = ?) x on x.billid=c.billid group by actorid order by c desc limit 11;";
+    break;  
   case 'stat':
     $sql = "select count(*) as c, YEAR(proposed_date) as y, MONTH(proposed_date) as m from CoActor c ";
     $sql .= " INNER JOIN Bill b on b.id = c.billid INNER Join Actor a on a.id=c.actorid ";
