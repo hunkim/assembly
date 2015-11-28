10565f138855b0e13ea87d0c08c33dd0177f74bc
Sung Kim <hunkim@gmail.com>
2015-11-28 22:22:47 +0800

    Added index

--- js/circle.js
+++ js/circle.js
@@ -14,8 +14,8 @@ var svg = d3.select("body").append("svg")
 
 
 d3.json(
- // "http://kassembly.xyz/circle.json",
- "http://api.kassembly.xyz/q.php/order",
+  "http://kassembly.xyz/circle.json",
+ //"http://api.kassembly.xyz/q.php/order",
  
   function(error, root) {
     //d3.json("all.json", function(error, root) {
