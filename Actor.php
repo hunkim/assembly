3356b372141b484a2f49eb5ecbd0efcbf5e9474d
Sung Kim <hunkim@gmail.com>
2015-11-30 12:26:39 +0800

    Added district

--- Actor.php
+++ Actor.php
@@ -2,9 +2,9 @@
 
 class Actor {
   var $id = false;
-  var $name;
-  var $cname;
-  var $party;
+  var $name="";
+  var $cname="";
+  var $party="";
 
   var $is_representative=0;
   var $is_proposer=0;
@@ -32,35 +32,47 @@ class Actor {
     $sql = "SELECT id from Actor where ";
     $sql .= "name_kr='" . $db->real_escape_string(($this->name)) . "'\n";
 
-    // dealing with empty name
-    if ($this->cname !== "") {
-      $sql .= "AND name_cn='" . $db->real_escape_string(($this->cname)) . "'\n";
+    // No party and cname, let's search for the same name only
+    if ($this->cname !== "" && $this->party !== "") {
+      if (($result=$db->query($sql)) === false) {
+        echo "Error: " . $sql . "\n" . $db->error;
+        exit(-1);
+      }
+
+      // if there is only one candidate, replace with existing one with cname and party
+      if ($result->num_rows == 1) {
+        // output data of each row
+        while($row = $result->fetch_assoc()) {
+          $this->id = intval($row["id"]);
+          echo("We have the actor with the same name: $this->id\n");
+          return true;
+        }
     }
 
-    // dealing with empty party
-    if ($this->party !== "") {
-      $sql .= "AND party='" . $db->real_escape_string(($this->party)) . "'\n";
-    }
+    // Add cname and party in the query
+    $sql .= "AND name_cn='" . $db->real_escape_string(($this->cname)) . "'\n";  
+    $sql .= "AND party='" . $db->real_escape_string(($this->party)) . "'\n";
 
-    echo $sql;
 
     if (($result=$db->query($sql)) === false) {
       echo "Error: " . $sql . "\n" . $db->error;
+      exit(-1);
+    }
+
+    // We need only one or 0 results.
+    assert($result->num_rows > 1,  "Cannot be more than one entry");
+    
+    // Not found
+    if ($result->num_rows == 0) {
       return false;
     }
+    
 
-    assert ($result->num_rows <= 1, "Cannot be grater than 1");
-
-    if ($result->num_rows == 1) {
-      // output data of each row
-      while($row = $result->fetch_assoc()) {
-        $this->id = intval($row["id"]);
-        echo("We have the actor: $this->id\n");
-        return true;
-      }
-    } else {
-        //echo "0 results";
-    }
+    while($row = $result->fetch_assoc()) {
+      $this->id = intval($row["id"]);
+      echo("We have the actor already: $this->id\n");
+      return true;
+    } 
 
     return false;
   }
